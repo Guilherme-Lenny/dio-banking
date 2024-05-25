193620a3726748b841cafdf4585b8d46bb9d21b6
Guilherme Lenny <guilherme_kenay@outlook.com>
2024-05-24 21:58:41 -0300

    first commit

---
+++ app.ts
@@ -0,0 +1,41 @@
+//Dio Banking
+
+//name, accountNumber
+//depositar, sacar
+
+abstract class Account {
+    name: string
+    accountNumber: number
+    balance: number = 0
+
+    constructor(name: string, accountNumber: number) {
+        this.name = name
+        this.accountNumber = accountNumber
+    }
+
+    deposit = () => {
+        console.log('Você depositou')
+    }
+
+    withdraw = () => {
+        console.log('Você sacou')
+    }    
+    
+    getBalance = () => {
+        console.log(this.balance)
+    }
+}
+
+class PeopleAccount extends Account {
+    doc_id:number
+
+    constructor(doc_id: number, name: string, accountNumber: number) {
+        super(name, accountNumber)
+        this.doc_id = doc_id
+    }
+}
+
+const peopleAccount: PeopleAccount = new PeopleAccount(10, 'Guilherme', 1)
+console.log(peopleAccount)
+
+
