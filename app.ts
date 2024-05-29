c2fc760c9bf2b59fd3b1d612ee415744b263eba3
Guilherme Lenny <guilherme_kenay@outlook.com>
2024-05-29 17:28:19 -0300

    update

--- app.ts
+++ app.ts
@@ -2,17 +2,12 @@ import { CompanyAccount } from './class/CompanyAccount'
 import { PeopleAccount } from './class/PeopleAccount'
 
 
-const peopleAccount: PeopleAccount = new PeopleAccount(10, 'Guilherme', 1)
-console.log(peopleAccount)
-peopleAccount.deposit()
+// const peopleAccount: PeopleAccount = new PeopleAccount(10, 'Guilherme', 1)
+// console.log(peopleAccount)
+// peopleAccount.deposit()
 
-/*
 const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-companyAccount.deposit()
-*/
+// companyAccount.deposit()
+console.log(companyAccount)
 
-/*
-const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-console.log(companyAccount.deposit())
-*/
 
